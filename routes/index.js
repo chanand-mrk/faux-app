9ae0669ce5f3c551664d297a43f7032f7b834b7f
nemetdan <dan.nemeth@merck.com>
2013-09-27 14:17:05 -0400

    Another POST attempt

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ exports.authcode = function(req, res) {
   var postOptions = {
     host : 'http://ddn4-test.apigee.net/v1/weather',
     port : 80,
-    path : '/oauth/token?grant_type=authorization_code&code=' + authcode + '&redirect_uri=http://faux-auth.herokuapp.com/authcode'
+    path : '/oauth/token?grant_type=authorization_code&code=' + authcode + '&redirect_uri=http://faux-auth.herokuapp.com/authcode',
     method : 'POST',
     headers : postHeaders
   };
