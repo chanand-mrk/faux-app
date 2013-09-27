7638557f3ce3e2d9e11bf2b486905182206d0873
nemetdan <dan.nemeth@merck.com>
2013-09-27 13:12:36 -0400

    Updated /authcode to request access token

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
+var publicKey = '5safzW8a9GABvYJruE9bIgWAPR11guHT';
+var secretKey = 'mC4TTQlzmyjP2aVG';
+var keyCode = (new Buffer(publicKey + secretKey).toString('base64'));
+
 exports.index = function(req, res) {
   res.render('index', { title: "Faux OAuth App" });
 };
 
 exports.login = function(req, res) {
-  res.redirect('http://ddn4-test.apigee.net/v1/weather/oauth/authorize?apikey=5safzW8a9GABvYJruE9bIgWAPR11guHT&response_type=code&scope=READ&state=foobar');
+  res.redirect('http://ddn4-test.apigee.net/v1/weather/oauth/authorize?apikey=' +
+               publicKey + '&response_type=code&scope=READ&state=foobar');
 };
 
 exports.authcode = function(req, res) {
-  res.end("<h2>AUTHCODE</h2><br><p>Once this screen appears, build out</p>");
+  // parse authcode
+  var authcode = req.query.code;
+
+  // post to token endpoint for access token
+  var http = require('http');
+  var token = http.createClient(80, 'http://ddn4-test.apigee.net/v1/weather');
+  var request = token.request('POST', 'oauth/token', 
+    { 'Authorization': 'Basic ' + keyCode,
+      'grant_type': 'authorization_code',
+      'code': authcode });
+  request.end();
+
+  request.on('response', function (tokenResponse) {
+    res.json(tokenResponse);
+  });
+  // save token (session?)
+  // display weather page
 };
